c8dc5c8bc88aefd8fda45aee5d3070622a3adb21
Guillermo Joaquin Vargas <vargasgjoaquin003@outlook.com>
2021-05-01 12:06:09 -0500

    Subiendo login

---
+++ Interfaces/login empleado/js/loginAdmin.js
@@ -0,0 +1,37 @@
+window.addEventListener('load', function(e){
+    document.getElementById('LoginAdmin').addEventListener('submit',enviodedatosfuncion);
+    });
+    
+    function enviodedatosfuncion(e)
+    {
+    e.preventDefault();
+    let usuario = document.getElementById('userAdmin').value;   
+    let password= document.getElementById('passAdmin').value;
+    JSONForm(usuario,password);
+    
+    return false;
+    }
+    
+    
+    function JSONForm(name, passwords){
+      const urllog = "http://localhost:8082/Administracion/Cabanas/LoginAdmin";
+    let JSONs = {
+        user_admin: name,
+        password_admin:passwords
+    }
+    
+    fetch(urllog,{
+      mode:"cors",
+      headers:{
+    "accept": "application/json",
+    "Content-Type":"application/json"
+      },
+    method:"POST",
+    body:JSON.stringify(JSONs)
+    ,
+    cache: "no-cache"
+    }).then(res => (res.text())
+    .then(res => (console.log(res))))
+    .catch(error => (console.log(error)))
+    
+    }
