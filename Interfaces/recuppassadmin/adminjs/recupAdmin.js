5f79475d62af88e5e9d4e8a7ac5fd1d8a5a056fb
Guillermo Joaquin Vargas <vargasgjoaquin003@outlook.com>
2021-05-10 17:15:04 -0500

    Subiendo interfaces de recuperar contras

--- Interfaces/recuppassadmin/adminjs/recupAdmin.js
+++ Interfaces/recuppassadmin/adminjs/recupAdmin.js
@@ -1,32 +1,32 @@
 
 window.addEventListener('load', function(e){
-     document.getElementById('FormularioContra').addEventListener('submit', CapturaDatos);
+    document.getElementById('FormularioAdmin').addEventListener('submit', CapturaDatos);
 
 })
 
 
 function CapturaDatos(e){
-    e.preventDefault();
+   e.preventDefault();
 
-let username = document.getElementById('usuarios').value;
-let contrasenas = document.getElementById('contra').value;
-let contrasenasNueva = document.getElementById('contraNueva').value;
+let username = document.getElementById('UserAdmin').value;
+let contrasenas = document.getElementById('PassAdmin').value;
+let contrasenasNueva = document.getElementById('PassAdminRe').value;
 
 JSONcontrasen(username,contrasenas,contrasenasNueva);
 }
 
 function JSONcontrasen(username, contrasenas, contrasenasNueva){
- const URLRecuperar = "http://localhost:8082/Administracion/Cabanas/Recuperar/ResetEmpleado";
-    
+const URLRecuperar = " http://localhost:8082/Administracion/Cabanas/Recuperar/ResetAdministrador";
+   
 let JSONContrasena={
 contrasena: contrasenas,
 confirmar_contrasena: contrasenasNueva
 }
 fetch(URLRecuperar +"/" +username, {
-  mode:"cors",
-  headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' }, 
-  method: 'POST', 
-  body: JSON.stringify(JSONContrasena)
+ mode:"cors",
+ headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' }, 
+ method: 'POST', 
+ body: JSON.stringify(JSONContrasena)
 }).then(res => (res.text())    // SI RESPONDE BIEN, DIGA UN LETRERO CON INNER HTML  "Contraseña restablecida con exito"
 .then(res => (console.log(res))))
 .catch(error => (console.log(error))); // SI RESPONDE MAL, DIGA UN LETRERO  "Usuario no encontrado, por favor intente de nuevo" igual con INNER HTML  
